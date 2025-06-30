19f75885a93e7c629d203404224c46daa3aaaad6
Md Shorif Uddin <mdshorifuddin463@gmail.com>
2025-06-30 23:53:35 +0600

    increment attendeecount dependency on client request

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
-
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
-
-// !Mongodb Database 
+// !Mongodb Database
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.56yvv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -22,103 +19,118 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // ! database collections 
-    const db =  client.db('Event-Nest-Db');
-    const userCollections = db.collection('users') // usercollection
-    const eventCollections = db.collection('events') // event collection
-
+    // ! database collections
+    const db = client.db("Event-Nest-Db");
+    const userCollections = db.collection("users"); // usercollection
+    const eventCollections = db.collection("events"); // event collection
 
     // user post api
 
-    app.post('/api/users', async(req,res) => {
-        const  newUserData = req.body;
-        const email = newUserData.email;  
-        
-        // varify user
-        if(!email){
-            return res.status(400).send({meassage : 'Email not found'})
-        }
+    app.post("/api/users", async (req, res) => {
+      const newUserData = req.body;
+      const email = newUserData.email;
 
-        // if User is already exist
-        const existingUser = await userCollections.findOne({email});
+      // varify user
+      if (!email) {
+        return res.status(400).send({ meassage: "Email not found" });
+      }
 
-        if(existingUser){
-            return res.status(200).send({meassage : 'User Already Exist in database'})
-        }
+      // if User is already exist
+      const existingUser = await userCollections.findOne({ email });
 
+      if (existingUser) {
+        return res
+          .status(200)
+          .send({ meassage: "User Already Exist in database" });
+      }
 
+      const result = await userCollections.insertOne(newUserData);
+      res.send(result);
+    });
 
-        const  result = await userCollections.insertOne(newUserData);
-             res.send(result)
-    })
-
-
-      // user get api
-      app.get('/api/users', async(req,res) => {
-  
-        const  result = await userCollections.find().toArray();
-             res.send(result)
-    })
-
+    // user get api
+    app.get("/api/users", async (req, res) => {
+      const result = await userCollections.find().toArray();
+      res.send(result);
+    });
 
     // ! event post api
-     app.post('/api/events', async(req,res) => {
-                 const eventData = req.body;
-                 
-                 const  result = await eventCollections.insertOne(eventData);
-                 res.send(result)
-     })
+    app.post("/api/events", async (req, res) => {
+      const eventData = req.body;
+
+      const result = await eventCollections.insertOne(eventData);
+      res.send(result);
+    });
 
     //  event get api
 
-    app.get('/api/events', async(req,res) => {
-                const result  = await eventCollections.find().toArray();
-                res.send(result)
-    })
+    app.get("/api/events", async (req, res) => {
+      const result = await eventCollections.find().toArray();
+      res.send(result);
+    });
 
     // event delete api
-    app.delete('/api/events/delete/:id', async(req,res) => {
-           const id  =  req.params.id;
-             console.log(id);
-             
-           const query = {_id : new ObjectId(id)};
+    app.delete("/api/events/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
 
-           const result = await eventCollections.deleteOne(query);
-           res.send(result);
-    })
+      const query = { _id: new ObjectId(id) };
+
+      const result = await eventCollections.deleteOne(query);
+      res.send(result);
+    });
 
     // event update api
-    app.patch('/api/events/update/:id', async(req,res) => {
-        const id = req.params.id;
-        const data = req.body;
-        const query = {_id : new ObjectId(id)};
-        
-        const updatedDoc = {
-           $set : {
-                 eventTitle : data.title,
-                 eventDate : data.date,
-                 eventTime : data.time,
-                 location : data.eventLoacation,
-                 description : data.eventDescription
-           }
+    app.patch("/api/events/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const query = { _id: new ObjectId(id) };
+      const email = data.email;
+
+      if (data.email) {
+        // check user already join events
+        const eventsItem = await eventCollections.findOne(query);
+        if (eventsItem.attendeeBy.includes(email)) {
+          return res
+            .status(400)
+            .send({ message: "you are already joined events" });
         }
 
-        const result = await eventCollections.updateOne(query,updatedDoc);
-        res.send(result)
-    })
+        // increment attendee count and push user email
+        const result = await eventCollections.updateOne(query, {
+          $inc: { attendeeCount: 1 },
+          $push: { attendeeBy: email },
+        });
 
+        res.send(result);
+      } else {
+        const updatedDoc = {
+          $set: {
+            eventTitle: data.title,
+            eventDate: data.date,
+            eventTime: data.time,
+            location: data.eventLoacation,
+            description: data.eventDescription,
+          },
+        };
 
+        const result = await eventCollections.updateOne(query, updatedDoc);
+        res.send(result);
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -126,21 +138,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", async (req, res) => {
+  res.send("event-nest server is running");
+});
 
-
-
-
-
-
-
-
-
-
-app.get('/',async(req,res) => {
-     res.send('event-nest server is running')
-})
-
-app.listen(port,() => {
-     console.log('event-nest server is running on port',port);
-     
-})
+app.listen(port, () => {
+  console.log("event-nest server is running on port", port);
+});
