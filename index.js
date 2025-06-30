5f4df189253437876e7c32ed46695a52c3cdd860
Md Shorif Uddin <mdshorifuddin463@gmail.com>
2025-06-30 16:50:06 +0600

    creat delete api to manage event delete requiest form clients

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 
@@ -81,6 +81,17 @@ async function run() {
     app.get('/api/events', async(req,res) => {
                 const result  = await eventCollections.find().toArray();
                 res.send(result)
+    })
+
+    // event delete api
+    app.delete('/api/events/delete/:id', async(req,res) => {
+           const id  =  req.params.id;
+             console.log(id);
+             
+           const query = {_id : new ObjectId(id)};
+
+           const result = await eventCollections.deleteOne(query);
+           res.send(result);
     })
 
 
